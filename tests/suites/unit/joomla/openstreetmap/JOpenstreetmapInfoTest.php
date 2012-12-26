b9c0b8168da00755166d75d97f7a3c6c8897ebb2
Buddhima <buddhimawijeweera@yahoo.com>
2012-12-26 13:29:44 +0530

    Info tests added

---
+++ tests/suites/unit/joomla/openstreetmap/JOpenstreetmapInfoTest.php
@@ -0,0 +1,253 @@
+<?php
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  Openstreetmap
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * Test class for JOpenstreetmapInfo.
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Openstreetmap
+ *
+ * @since       12.3
+ */
+class JOpenstreetmapInfoTest extends TestCase
+{
+	/**
+	 * @var    JRegistry  Options for the Openstreetmap object.
+	 * @since  12.3
+	 */
+	protected $options;
+
+	/**
+	 * @var    JHttp  Mock client object.
+	 * @since  12.3
+	 */
+	protected $client;
+
+	/**
+	 * @var    JInput The input object to use in retrieving GET/POST data.
+	 * @since  12.3
+	 */
+	protected $input;
+
+	/**
+	 * @var    JOpenstreetmapInfo Object under test.
+	 * @since  12.3
+	 */
+	protected $object;
+
+	/**
+	 * @var    JOpenstreetmapOauth  Authentication object for the Openstreetmap object.
+	 * @since  12.3
+	 */
+	protected $oauth;
+
+	/**
+	 * @var    string  Sample JSON string.
+	 * @since  12.3
+	 */
+	protected $sampleString = '<osm><changeset></changeset></osm>';
+
+	/**
+	 * @var    string  Sample JSON error message.
+	 * @since  12.3
+	 */
+	protected $errorString = '{"error":"Generic error"}';
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 *
+	 * @return void
+	 */
+	protected function setUp()
+	{
+		$_SERVER['HTTP_HOST'] = 'example.com';
+		$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0';
+		$_SERVER['REQUEST_URI'] = '/index.php';
+		$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+		$key = "app_key";
+		$secret = "app_secret";
+			
+		$access_token = array('key' => 'token_key', 'secret' => 'token_secret');
+
+		$this->options = new JRegistry;
+		$this->input = new JInput;
+		$this->client = $this->getMock('JHttp', array('get', 'post', 'delete', 'put'));
+		$this->oauth = new JOpenstreetmapOauth($this->options, $this->client, $this->input);
+		$this->oauth->setToken($access_token);
+
+		$this->object = new JOpenstreetmapInfo($this->options, $this->client, $this->oauth);
+
+		$this->options->set('consumer_key', $key);
+		$this->options->set('consumer_secret', $secret);
+		$this->options->set('sendheaders', true);
+	}
+
+	/**
+	 * Tests the getCapabilities method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.3
+	 */
+	public function testGetCapabilities()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+
+		$path = 'capabilities';
+
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->assertThat(
+				$this->object->getCapabilities(),
+				$this->equalTo(new SimpleXMLElement(''))
+		);
+	}
+
+	/**
+	 * Tests the getCapabilities method - failure
+	 *
+	 * @return  void
+	 *
+	 * @since   12.3
+	 */
+	public function testGetCapabilitiesFailure()
+	{
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+	
+		$path = 'capabilities';
+	
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+	
+		$this->object->getCapabilities();
+	}
+
+	/**
+	 * Tests the retrieveMapData method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.3
+	 */
+	public function testRetrieveMapData()
+	{
+		$left = '1';
+		$bottom = '1';
+		$right = '2';
+		$top = '2';
+
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+	
+		$path = 'map?bbox=' . $left . ',' . $bottom . ',' . $right . ',' . $top;
+	
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+	
+		$this->assertThat(
+				$this->object->retrieveMapData($left, $bottom, $right, $top),
+				$this->equalTo(new SimpleXMLElement(''))
+		);
+	}
+
+	/**
+	 * Tests the retrieveMapData method - failure
+	 *
+	 * @return  void
+	 *
+	 * @since   12.3
+	 */
+	public function testRetrieveMapDataFailure()
+	{
+		$left = '1';
+		$bottom = '1';
+		$right = '2';
+		$top = '2';
+
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+
+		$path = 'map?bbox=' . $left . ',' . $bottom . ',' . $right . ',' . $top;
+
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->object->retrieveMapData($left, $bottom, $right, $top);
+	}
+
+	/**
+	 * Tests the retrievePermissions method
+	 *
+	 * @return  void
+	 *
+	 * @since   12.3
+	 */
+	public function testRetrievePermissions()
+	{
+
+		$returnData = new stdClass;
+		$returnData->code = 200;
+		$returnData->body = $this->sampleString;
+
+		$path = 'permissions';
+
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->assertThat(
+				$this->object->retrievePermissions(),
+				$this->equalTo(new SimpleXMLElement(''))
+		);
+	}
+
+	/**
+	 * Tests the retrievePermissions method - failure
+	 *
+	 * @return  void
+	 *
+	 * @since   12.3
+	 */
+	public function testRetrievePermissionsFailure()
+	{
+
+		$returnData = new stdClass;
+		$returnData->code = 500;
+		$returnData->body = $this->errorString;
+
+		$path = 'permissions';
+
+		$this->client->expects($this->once())
+		->method('get')
+		->with($path)
+		->will($this->returnValue($returnData));
+
+		$this->object->retrievePermissions();
+	}
+}
